1dc923f34b943d5659353a1fced840d44f0158cb
Hans Boden <h.boden@prc-boden.de>
2016-05-25 16:01:27 +0100

    Sample solution for the HanoiTower puzzle, room plan for the Mad-Hotel

---
+++ projects/mad_hotel/mhrooms..py
@@ -0,0 +1,15 @@
+# python3
+"""
+    Build the room plan for the Mad Hotel
+    Read the data from the room-plan text file
+"""
+
+def main():
+    pass
+
+
+class Room():
+    pass
+
+if __name__ == '__main__':
+    main
